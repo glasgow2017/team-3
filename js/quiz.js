9f2f15d8ee4fc4bcc148fbc1a0cf9be42331299b
Noah Florin-Sefton <n.fs@hotmail.co.uk>
2017-11-04 18:54:02 +0000

    Add files via upload

---
+++ js/quiz.js
@@ -0,0 +1,17 @@
+$(function() {
+
+
+})
+
+/*<div class="question">
+        <p class="ask-question">A crocodile is anothger word for a dolphin ? </p>
+        <div class="answer-question radio radio-group">
+            <label><input type="radio" name="question1">True</label>
+        </div>
+        <div class="radio">
+            <label><input type="radio" name="question1">False</label>
+        </div>
+        <div class="radio">
+            <label><input type="radio" name="question1">Dont know</label>
+        </div>
+    </div>*/
