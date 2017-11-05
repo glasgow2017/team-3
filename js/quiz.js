b8b4bd9cecaf313120d5d31ee060cbb20fe1a176
donald108 <noreply@donald108.com>
2017-11-05 07:47:12 +0000

    Cleaned up js and changed CSS

--- js/quiz.js
+++ js/quiz.js
@@ -29,77 +29,43 @@ $.get('json/questions.json',function(response) {
 
     $('#submitBt').on('click',function(e) {
         e.preventDefault();
-                var resultsArr = [];
-                for (var p = 0;p < 10;p++) {
-                    $('.question' + p + ' input').each(function (  index ) {
-                    if ($(this).is(':checked')) {
-                        if (index == 0 ) {
-                            resultsArr[p] = {"id" : p, "result" : true}
-                        }
-                        else {
-                           resultsArr[p] = {"id" : p, "result" : false}
-                        }
-                    }
-
-                    })
-                }
-             console.log(resultsArr);
-             var region = $('#regionIn:selected').val();
-             var age = $('#ageIn').val();
-             var gender = $('#genderIn:selected').val();
-             var data = {  'gender':gender,
-                           'age':age,
-                           'region':region,
-                           'results':resultsArr }
-            $.post('cgi-bin/processForm',data)
-          })
-
-
-
-
-    $('#submitBt').on('click',function(e) {
-    e.preventDefault();
-            var resultsArr = [];
-            for (var p = 0;p < 10;p++) {
-                $('.question' + p + ' input').each(function ( index ) {
+        var resultsArr = [];
+        for (var p = 0;p < 10;p++) {
+            $('.question' + p + ' input').each(function (  index ) {
                 if ($(this).is(':checked')) {
                     if (index == 0 ) {
                         resultsArr[p] = {"id" : p, "result" : true}
-                    }
-                    else {
-                       resultsArr[p] = {"id" : p, "result" : false}
+                    } else {
+                        resultsArr[p] = {"id" : p, "result" : false}
                     }
                 }
-
-                })
-            }
-
-         console.log(resultsArr);
-         var region = $('#regionIn:selected').val();
-         var age = $('#ageIn').val();
-         var gender = $('#genderIn:selected').val();
-         var data = {  'gender':gender,
-                       'age':age,
-                       'region':region,
-                       'results':resultsArr }
+            });
+        }
+        console.log(resultsArr);
+        var region = $('#regionIn:selected').val();
+        var age = $('#ageIn').val();
+        var gender = $('#genderIn:selected').val();
+        var data = {  'gender':gender,
+            'age':age,
+            'region':region,
+            'results':resultsArr };
         $.post('cgi-bin/processForm',data)
-      })
+    });
 
-       $('input').on('click', function (event) {
-             var group = $(this).attr('name')
+    $('input').on('click', function (event) {
+        var group = $(this).attr('name');
 
-              $('.question' + group + ' input').each(function( index ) {
-                $(this).attr('disabled','true')
-                 if ($(this).is(':checked')) {
-                     if (index == 0 ) {
-                        $('.question' + group).addClass('correct')
-                      }
-                      else {
-                        $('.question' + group).addClass('false')
-                        }
-                  }
-              })
-       });
+        $('.question' + group + ' input').each(function( index ) {
+            $(this).attr('disabled','true')
+            if ($(this).is(':checked')) {
+                if (index == 0 ) {
+                    $('.question' + group).addClass('correct')
+                } else {
+                    $('.question' + group).addClass('false')
+                }
+            }
+        });
+    });
 
 });
 
